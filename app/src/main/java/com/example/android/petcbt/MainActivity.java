27d1769a5442435d28dc140eec4c7eb4ffbd7cfa
Brian Lee <brianhylee99@gmail.com>
2017-09-16 18:15:03 -0700

    Mock list of positive and negative words

--- app/src/main/java/com/example/android/petcbt/MainActivity.java
+++ app/src/main/java/com/example/android/petcbt/MainActivity.java
@@ -2,19 +2,26 @@ package com.example.android.petcbt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView catView;
+//    private EditText describeFeeling;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         catView = (ImageView) findViewById(R.id.cat);
+//        describeFeeling = (EditText) findViewById(R.id.describe_feeling);
     }
 
+
+
     // TODO: implement cat changing
+
 
 }
